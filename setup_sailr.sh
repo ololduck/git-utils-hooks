0f1d965b8d3a788f0f9ea12b34c79fa5246c1cde
Paul Ollivier <contact@paulollivier.fr>
2021-12-19 11:51:32 +0100

    fix: I forgot we were on curl and not wget u_u

--- setup_sailr.sh
+++ setup_sailr.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e # exit on error
-curl -O .git/sailr.sh \
-	https://raw.githubusercontent.com/craicoverflow/sailr/master/sailr.sh
+curl https://raw.githubusercontent.com/craicoverflow/sailr/master/sailr.sh > .git/sailr.sh
 chmod +x .git/sailr.sh
 if [ -f sailr.json ]; then
-	curl -O sailr.sh \
-		https://raw.githubusercontent.com/craicoverflow/sailr/master/sailr.json
+	curl https://raw.githubusercontent.com/craicoverflow/sailr/master/sailr.json > sailr.json
 fi
 git stash
 git add sailr.json
